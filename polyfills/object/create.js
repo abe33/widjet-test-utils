1f31db5a06708e937b151f714d7088afb6c60ba8
abe33 <cedric.nehemie@gmail.com>
2016-10-11 16:42:31 +0200

    Allow null as first argument of Object.create shim

--- polyfills/object/create.js
+++ polyfills/object/create.js
@@ -1,11 +1,5 @@
 if (!Object.create) {
-  Object.create = function (o, properties) {
-    if (typeof o !== 'object' && typeof o !== 'function') {
-      throw new TypeError('Object prototype may only be an Object: ' + o)
-    } else if (o === null) {
-      throw new Error("This browser's implementation of Object.create is a shim and doesn't support 'null' as the first argument.")
-    }
-
+  Object.create = function (o = {}, properties) {
     function F () {}
 
     F.prototype = o
